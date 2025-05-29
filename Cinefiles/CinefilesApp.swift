f620f398ea58d9d509ba3fdb47f85a0a9cb21f3d
rafaelvenetikides <rafavenetikides@gmail.com>
2025-05-29 10:54:39 -0300

    Initial Commit

---
+++ Cinefiles/CinefilesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CinefilesApp.swift
+//  Cinefiles
+//
+//  Created by Rafael Venetikides on 29/05/25.
+//
+
+import SwiftUI
+
+@main
+struct CinefilesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
