d60e428bc447a7152a35a9d0d6e17a46cd294a6e
luizblank <luizrosacardoso@gmail.com>
2025-05-29 11:30:33 -0300

    Added button component

---
+++ Cinefiles/View/Components/Button.swift
@@ -0,0 +1,20 @@
+import SwiftUI
+
+struct Button : View {
+    var text : String
+    var color : Color
+    
+    var body : some View {
+        VStack {
+            Text(text)
+                .font(.system(size: 18, weight: .semibold))
+        }
+        .frame(maxWidth: .infinity)
+        .padding(.vertical, 12)
+        .background(color)
+    }
+}
+
+#Preview {
+    Button(text: "Botão", color: .blue)
+}
