1f916e7f92f9f1ae491f2fce2412b83338ad5926
luizblank <luizrosacardoso@gmail.com>
2025-05-30 11:45:31 -0300

    Modified button component

--- Cinefiles/View/Components/CustomButtonView.swift
+++ Cinefiles/View/Components/CustomButtonView.swift
@@ -2,15 +2,20 @@ import SwiftUI
 
 struct CustomButtonView : View {
     @Binding var crrPage : Page
-    
+
     var text : String
     var color : Color
     var destinationView : Page
-    
+    var icon : String = ""
+
     var body : some View {
         Button {
             crrPage = destinationView
         } label: {
+            if icon != "" {
+                Image(systemName: icon)
+                    .foregroundStyle(.black)
+            }
             Text(text)
                 .font(.system(size: 18, weight: .semibold))
                 .foregroundStyle(.black)
