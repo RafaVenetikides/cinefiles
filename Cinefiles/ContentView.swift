00871e940d94998a1ba5e5d467bc185e74938c29
luizblank <luizrosacardoso@gmail.com>
2025-05-30 12:57:58 -0300

    Fixed profile navigation, removed navigation system

--- Cinefiles/ContentView.swift
+++ Cinefiles/ContentView.swift
@@ -1,32 +1,30 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var crrPage : Page = .login
-    
     var body: some View {
         VStack {
-            Group {
-                switch crrPage {
-                case .home :
-                    Text("Home")
-                case .onboarding:
-                    OnBoardingView(crrPage: $crrPage)
-                case .login:
-                    LoginView(crrPage: $crrPage)
-                case .perfil:
-                    VStack {
-                        Text("PERFIL\n🐴")
-                            .font(.system(size: 34, weight: .semibold))
-                            .foregroundStyle(.white)
-                            .multilineTextAlignment(.center)
-                    }
-                    .frame(
-                        maxWidth: .infinity,
-                        maxHeight: .infinity
-                    )
-                    .background(.customDarkBlue)
-                }
-            }
+//            Group {
+//                switch crrPage {
+//                case .home :
+//                    Text("Home")
+//                case .onboarding:
+//                    OnBoardingView(crrPage: $crrPage)
+//                case .login:
+//                    LoginView(crrPage: $crrPage)
+//                case .perfil:
+//                    VStack {
+//                        Text("PERFIL\n🐴")
+//                            .font(.system(size: 34, weight: .semibold))
+//                            .foregroundStyle(.white)
+//                            .multilineTextAlignment(.center)
+//                    }
+//                    .frame(
+//                        maxWidth: .infinity,
+//                        maxHeight: .infinity
+//                    )
+//                    .background(.customDarkBlue)
+//                }
+//            }
         }
     }
 }
